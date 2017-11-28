dc4f60e1c428cfcb0e5ac1e529eff25d29334fa1
eoin <eoinpboylan@gmail.com>
2017-11-28 13:29:38 +0000

    Added class for the Dwarf

---
+++ 03-01-b-DwarfsAndDwarves/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Dwarves are Dwarfs!!!
+		ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+		
+		Dwarf sleepy = new Dwarf();
+		sleepy.setName("Sleepy");
+		sleepy.setAuthor("Disney");
+		sleepy.setImage("Sleepy.png");
+		
+		Dwarf happy = new Dwarf();
+		happy.setName("Happy");
+		happy.setAuthor("Disney");
+		happy.setImage("Happy.png");
+
+		Dwarf grumpy = new Dwarf();
+		grumpy.setName("Grumpy");
+		grumpy.setAuthor("Disney");
+		grumpy.setImage("Grumpy.png");
+		
+		
+		
+	}
+
+}
